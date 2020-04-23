357bf99647593a89164bbdd5bcde429910704873
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-04-23 14:52:44 -0700

    .

---
+++ src/slow-start.js
@@ -0,0 +1,167 @@
+const cooler = require("./ssb")({offline: true});
+const pull = require("pull-stream");
+
+// A map (like an object) where the values are sets (like arrays).
+//
+// If you convert the map to an object and sets to arrays, it'd look like:
+//
+//     { "a": [0, 1, 2], "b": [3, 4, 5] }
+//
+// For example, in `messagesByType` the map keys are message keys and the set
+// is a list of message keys that have that type.
+class MapSet extends Map {
+  add(key, item) {
+    if (this.has(key) === false) {
+      this.set(key, new Set());
+    }
+
+    return this.get(key).add(item);
+  }
+}
+
+const messagesByType = new MapSet();
+const messagesByAuthor = new MapSet();
+const latestSeqByAuthor = new Map();
+const rootsReferencedByAuthor = new MapSet();
+const nameByAuthor = new Map();
+const imageByAuthor = new Map();
+const descriptionByAuthor = new Map();
+const notificationsForAuthor = new MapSet(); // only for me
+const messagesBySubstring = new MapSet();
+const followingByAuthor = new MapSet();
+const blockingByAuthor = new MapSet();
+
+const isVisible = (messageValueContent) =>
+  typeof messageValueContent === "object";
+
+// Connect to the SSB service and start the stream.
+cooler.open().then((ssb) => {
+  // This function runs on every message, and populates the indexes. This seems
+  // to be super fast, and my computer (2015 Chromebook Pixel) can process 1
+  // million messages in ~55 seconds.
+  console.log('============== starting indexing ==============');
+  let startTime = Date.now();
+  const onEach = (message) => {
+    const { key } = message;
+    const { author, seq } = message.value;
+
+    messagesByAuthor.add(author, key);
+    latestSeqByAuthor.set(author, seq);
+
+    if (isVisible(message.value.content)) {
+      const { type } = message.value.content;
+
+      messagesByType.add(type, key);
+
+      switch (type) {
+        case "contact": {
+          const { contact, following, blocking } = message.value.content;
+          if (contact != null) {
+            if (following) {
+              followingByAuthor.add(author, contact);
+            }
+            if (blocking) {
+              blockingByAuthor.add(author, contact);
+            }
+          }
+
+          break;
+        }
+        case "about": {
+          const { name, description, image, about } = message.value.content;
+          if (about != null && about === author) {
+            if (name != null) {
+              nameByAuthor.set(author, name);
+            }
+            if (description != null) {
+              descriptionByAuthor.set(author, description);
+            }
+            if (image != null) {
+              imageByAuthor.set(author, image);
+            }
+          }
+          break;
+        }
+        case "post": {
+          const { root, mentions, text } = message.value.content;
+          const isValidRoot = typeof root === "string";
+
+          if (typeof text === "string" && text.length >= 3) {
+            const words = text.match(/\w{3,}/g) || [];
+            words.forEach((word) =>
+              messagesBySubstring.add(word.toLowerCase(), key)
+            );
+          }
+
+          // Only index conversations that I'm a part of.
+          if (isValidRoot && author === ssb.id) {
+            rootsReferencedByAuthor.add(author, root);
+          }
+
+          const mentionsMe =
+            Array.isArray(mentions) &&
+            mentions.map((x) => x.link).includes(ssb.id);
+
+          if (mentionsMe) {
+            notificationsForAuthor.add(ssb.id, key);
+          } else {
+            if (isValidRoot) {
+              if (rootsReferencedByAuthor.has(ssb.id)) {
+                if (rootsReferencedByAuthor.get(ssb.id).has(root)) {
+                  notificationsForAuthor.add(ssb.id, key);
+                }
+              }
+            }
+          }
+
+          break;
+        }
+      }
+    }
+  };
+
+  // After the stream is finished, print some debugging information.
+  const onDone = (err) => {
+    console.log('============== finished indexing ==============');
+    let endTime = Date.now();
+    let seconds = (endTime - startTime) / 1000
+    console.log('Elapsed time: ' + seconds + ' seconds');
+
+    if (err) throw err;
+
+    console.log({
+      name: nameByAuthor.get(ssb.id),
+      description: descriptionByAuthor.get(ssb.id),
+      image: imageByAuthor.get(ssb.id),
+      notifications: notificationsForAuthor.get(ssb.id).size,
+    });
+
+    console.log(
+      "Number of posts with the word pizza:",
+      messagesBySubstring.get("pizza").size
+    );
+
+    const used = process.memoryUsage();
+    for (let key in used) {
+      console.log(
+        `${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`
+      );
+    }
+
+    // Run with:
+    // - `node inspect better.js`
+    // - Start with `c` (for "continue")
+    // - Wait until the debugger hits our breakpoint
+    // - Type `repl` and take a look at the indexes.
+    debugger;
+
+    ssb.close();
+  };
+
+  // Start the stream!
+  pull(
+      ssb.createLogStream({limit: 1000}),
+      pull.drain(onEach, onDone)
+  );
+});
+
---
+++ src/ssb.js
@@ -0,0 +1,254 @@
+"use strict";
+
+// This module exports a function that connects to SSB and returns an interface
+// to call methods over MuxRPC. It's a thin wrapper around SSB-Client, which is
+// a thin wrapper around the MuxRPC module.
+
+const { promisify } = require("util");
+const ssbClient = require("ssb-client");
+const ssbConfig = require("ssb-config");
+const flotilla = require("@fraction/flotilla");
+const ssbTangle = require("ssb-tangle");
+const debug = require("debug")("oasis");
+const path = require("path");
+const pull = require("pull-stream");
+const lodash = require("lodash");
+
+const socketPath = path.join(ssbConfig.path, "socket");
+const publicInteger = ssbConfig.keys.public.replace(".ed25519", "");
+const remote = `unix:${socketPath}~noauth:${publicInteger}`;
+
+// This is unnecessary when https://github.com/ssbc/ssb-config/pull/72 is merged
+ssbConfig.connections.incoming.unix = [
+  { scope: "device", transform: "noauth" },
+];
+
+/**
+ * @param formatter {string} input
+ * @param args {any[]} input
+ */
+const log = (formatter, ...args) => {
+  const isDebugEnabled = debug.enabled;
+  debug.enabled = true;
+  debug(formatter, ...args);
+  debug.enabled = isDebugEnabled;
+};
+
+/**
+ * @param [options] {object} - options to pass to SSB-Client
+ * @returns Promise
+ */
+const connect = (options) =>
+  new Promise((resolve, reject) => {
+    const onSuccess = (api) => {
+      if (api.tangle === undefined) {
+        // HACK: SSB-Tangle isn't available in Patchwork, but we want that
+        // compatibility. This code automatically injects SSB-Tangle into our
+        // stack so that we don't get weird errors when using Patchwork.
+        //
+        // See: https://github.com/fraction/oasis/issues/21
+        api.tangle = ssbTangle.init(api);
+
+        // MuxRPC supports promises but the raw plugin does not.
+        api.tangle.branch = promisify(api.tangle.branch);
+      }
+
+      resolve(api);
+    };
+
+    ssbClient(null, options).then(onSuccess).catch(reject);
+  });
+
+let closing = false;
+let serverHandle;
+let clientHandle;
+
+/**
+ * Attempts connection over Unix socket, falling back to TCP socket if that
+ * fails. If the TCP socket fails, the promise is rejected.
+ * @returns Promise
+ */
+const attemptConnection = () =>
+  new Promise((resolve, reject) => {
+    connect({ remote })
+      .then((ssb) => {
+        debug("Connected to existing Scuttlebutt service over Unix socket");
+        resolve(ssb);
+      })
+      .catch((e) => {
+        if (closing) return;
+        debug("Unix socket failed");
+        if (e.message !== "could not connect to sbot") {
+          throw e;
+        }
+        connect()
+          .then((ssb) => {
+            log("Connected to existing Scuttlebutt service over TCP socket");
+            resolve(ssb);
+          })
+          .catch((e) => {
+            if (closing) return;
+            debug("TCP socket failed");
+            if (e.message !== "could not connect to sbot") {
+              throw e;
+            }
+            reject(new Error("Both connection options failed"));
+          });
+      });
+  });
+
+let pendingConnection = null;
+
+const ensureConnection = (customConfig) => {
+  if (pendingConnection === null) {
+    pendingConnection = new Promise((resolve) => {
+      attemptConnection()
+        .then((ssb) => {
+          resolve(ssb);
+        })
+        .catch(() => {
+          debug("Connection attempts to existing Scuttlebutt services failed");
+          log("Starting Scuttlebutt service");
+
+          // Start with the default SSB-Config object.
+          const server = flotilla(ssbConfig);
+          // Adjust with `customConfig`, which declares further preferences.
+          serverHandle = server(customConfig);
+
+          // Give the server a moment to start. This is a race condition. :/
+          setTimeout(() => {
+            attemptConnection()
+              .then((ssb) => {
+                autoStagePeers({ ssb, config: customConfig });
+                resolve(ssb);
+              })
+              .catch((e) => {
+                throw new Error(e);
+              });
+          }, 100);
+        });
+    });
+
+    const cancel = () => (pendingConnection = null);
+    pendingConnection.then(cancel, cancel);
+  }
+
+  return pendingConnection;
+};
+
+const autoStagePeers = ({ ssb, config }) => {
+  // TODO: This does not start when Oasis is started in --offline mode, which
+  // is great, but if you start Oasis in --offline mode and select 'Start
+  // networking' then this doesn't come into play.
+  //
+  // The right place to fix this is in the scheduler, and this entire function
+  // should be replaced by: https://github.com/staltz/ssb-conn/pull/17
+  if (config.conn.autostart !== true) {
+    return;
+  }
+
+  const inProgress = {};
+  const maxHops = lodash.get(
+    ssbConfig,
+    "friends.hops",
+    lodash.get(ssbConfig, "friends.hops", 0)
+  );
+
+  const add = (address) => {
+    inProgress[address] = true;
+    return () => {
+      inProgress[address] = false;
+    };
+  };
+
+  ssb.friends.hops().then((hops) => {
+    pull(
+      ssb.conn.stagedPeers(),
+      pull.drain((x) => {
+        x.filter(([address, data]) => {
+          const notInProgress = inProgress[address] !== true;
+
+          const key = data.key;
+          const haveHops = typeof hops[key] === "number";
+          const hopValue = haveHops ? hops[key] : Infinity;
+          // Negative hops means blocked
+          const isNotBlocked = hopValue >= 0;
+          const withinHops = isNotBlocked && hopValue <= maxHops;
+
+          return notInProgress && withinHops;
+        }).forEach(([address, data]) => {
+          const done = add(address);
+          debug(
+            `Connecting to staged peer at ${
+              hops[data.key]
+            }/${maxHops} hops: ${address}`
+          );
+          ssb.conn.connect(address, data).then(done).catch(done);
+        });
+      })
+    );
+  });
+};
+
+module.exports = ({ offline }) => {
+  if (offline) {
+    log("Offline mode activated - not connecting to scuttlebutt peers or pubs");
+    log(
+      "WARNING: Oasis can connect to the internet through your other SSB apps if they're running."
+    );
+  }
+
+  const customConfig = {
+    conn: {
+      autostart: !offline,
+    },
+  };
+
+  /**
+   * This is "cooler", a tiny interface for opening or reusing an instance of
+   * SSB-Client.
+   */
+  const cooler = {
+    open() {
+      // This has interesting behavior that may be unexpected.
+      //
+      // If `clientHandle` is already an active [non-closed] connection, return that.
+      //
+      // If the connection is closed, we need to restart it. It's important to
+      // note that if we're depending on an external service (like Patchwork) and
+      // that app is closed, then Oasis will seamlessly start its own SSB service.
+      return new Promise((resolve, reject) => {
+        if (clientHandle && clientHandle.closed === false) {
+          resolve(clientHandle);
+        } else {
+          ensureConnection(customConfig).then((ssb) => {
+            clientHandle = ssb;
+            if (closing) {
+              cooler.close();
+              reject(new Error("Closing Oasis"));
+            } else {
+              resolve(ssb);
+            }
+          });
+        }
+      });
+    },
+    close() {
+      closing = true;
+      if (clientHandle && clientHandle.closed === false) {
+        clientHandle.close();
+      }
+      if (serverHandle) {
+        serverHandle.close();
+      }
+    },
+  };
+
+  // Important: This ensures that we have an SSB connection as soon as Oasis
+  // starts. If we don't do this, then we don't even attempt an SSB connection
+  // until we receive our first HTTP request.
+  cooler.open();
+
+  return cooler;
+};
+
